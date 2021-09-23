4dd7890b6f440220a0d1ebfaa61a5528019c85b4
nielvid <nielvidservices@gmail.com>
2021-09-23 02:25:26 +0100

    feat: 🎸 Peer Server set up to broker connection
    Peer Server setup to brokker connection between peers

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
-import express from 'express';
-// rest of the code remains same
-const app = express();
-const PORT = 8000;
-app.get('/', (req, res, next) => res.send('Express Server is not runnning  fine'));
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const { ExpressPeerServer } = require('peer')
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.listen(4000)
+const uuid = require('uuid')
+
+app.use(express.json())
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    // credentials: true
+  })
+)
+
+io.on('connection', (socket) => {
+  // console.log(socket.id)
+  const roomId = uuid.v4()
+  socket.emit('connection', roomId)
+
+  socket.on('join-room', (data) => {
+    const { room, userId } = data
+    socket.join(roomId)
+    socket.to(userId).emit('user-connected', userId)
+    console.log(room, userId)
+  })
+})
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+})
+
+app.use('/peerjs', peerServer)
+
+peerServer.on('connection', (client) => {
+  console.log(client.id, 'client connected')
+})
+peerServer.on('disconnect', (client) => {
+  console.log(client.id, 'client disconnected')
+})
+
+app.get('/', (req, res, next) =>
+  res.send('Express Server is not runnning  fine')
+)
+
+app.get('/:room', (req, res) => {
+  // const id = uuidv4()
+  const { room } = req.params
+  res.send(room)
+})
+
+const PORT = 8000
+server.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+})
